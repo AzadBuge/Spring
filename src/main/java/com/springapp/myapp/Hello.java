d07a55d9a75bc7dcd5acc7261e31fca409f38e12
azad buge <azad@azads-MacBook-Air.local>
2026-03-30 15:52:10 +0530

    /Hello.java adding the @Component so that it will know its the Spring Component

--- src/main/java/com/springapp/myapp/Hello.java
+++ src/main/java/com/springapp/myapp/Hello.java
@@ -1,7 +1,11 @@
 package com.springapp.myapp;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Hello {
     public String build(){
+        System.out.println("In Build");
         return "Spring Boot Developer Azad (Learner) ..!";
     }
 }
--- src/main/java/com/springapp/myapp/MyappApplication.java
+++ src/main/java/com/springapp/myapp/MyappApplication.java
@@ -4,18 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-
 @SpringBootApplication
 public class MyappApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context;
-		//we are using teh object of 
-		// SpringApplication.run(MyappApplication.class, args);
-		// and using it because the object which is created will not be created at JVM
-		// it will be created at the IoC container
 
-		context = SpringApplication.run(MyappApplication.class, args); //Loads at IoC container in JVM
+		ApplicationContext context =
+				SpringApplication.run(MyappApplication.class, args);
+		Hello obj = context.getBean(Hello.class);
+		obj.build();
 	}
 
 }
