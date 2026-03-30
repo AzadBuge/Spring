eec550a7a91234d9cd16d84bea73fa440704d9d3
azad buge <azad@azads-MacBook-Air.local>
2026-03-30 16:32:55 +0530

    use of @Primary to solve the bytype confusion

--- src/main/java/com/springapp/myapp/DeskTop.java
+++ src/main/java/com/springapp/myapp/DeskTop.java
@@ -1,8 +1,10 @@
 package com.springapp.myapp;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // use of primary to solve the confusion of by type
 public class DeskTop implements ComputerInterFace {
     public void pc() {
         System.out.println("DeskTop");
