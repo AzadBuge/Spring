514519e2559ee3149648f7a051d17150470d25ad
azad buge <azad@azads-MacBook-Air.local>
2026-03-30 16:05:57 +0530

    Autowired means connecting the components without creating an extra object

--- src/main/java/com/springapp/myapp/Hello.java
+++ src/main/java/com/springapp/myapp/Hello.java
@@ -1,11 +1,15 @@
 package com.springapp.myapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Hello {
-    public String build(){
+
+    @Autowired // connects the component Hello and LapTop
+    private LapTop lapTop ;
+    public void build(){
         System.out.println("In Build");
-        return "Spring Boot Developer Azad (Learner) ..!";
+        lapTop.pc();
     }
 }
---
+++ src/main/java/com/springapp/myapp/LapTop.java
@@ -0,0 +1,10 @@
+package com.springapp.myapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LapTop {
+    public void pc(){
+        System.out.println("/LapTop/pc..");
+    }
+}
