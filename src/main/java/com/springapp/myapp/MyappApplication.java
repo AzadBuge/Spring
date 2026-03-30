ce8a68b4cf5f4ccaaedc4e0878faae09b189a4c5
azad buge <azad@azads-MacBook-Air.local>
2026-03-30 15:41:52 +0530

    Initializing the Object to SpringApplication obj (IoC container)

--- src/main/java/com/springapp/myapp/MyappApplication.java
+++ src/main/java/com/springapp/myapp/MyappApplication.java
@@ -9,12 +9,13 @@ import org.springframework.context.ApplicationContext;
 public class MyappApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context; //we are using teh object of 
+		ApplicationContext context;
+		//we are using teh object of 
 		// SpringApplication.run(MyappApplication.class, args);
 		// and using it because the object which is created will not be created at JVM
 		// it will be created at the IoC container
 
-		SpringApplication.run(MyappApplication.class, args); //Loads at IoC container in JVM
+		context = SpringApplication.run(MyappApplication.class, args); //Loads at IoC container in JVM
 	}
 
 }
