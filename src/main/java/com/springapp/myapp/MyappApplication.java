cf1897b281b6f4f4504234f78d803518a4e34c01
azad buge <azad@azads-MacBook-Air.local>
2026-03-30 15:39:57 +0530

    ApplicationContext object is declared

--- src/main/java/com/springapp/myapp/MyappApplication.java
+++ src/main/java/com/springapp/myapp/MyappApplication.java
@@ -2,12 +2,19 @@ package com.springapp.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class MyappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyappApplication.class, args);
+		ApplicationContext context; //we are using teh object of 
+		// SpringApplication.run(MyappApplication.class, args);
+		// and using it because the object which is created will not be created at JVM
+		// it will be created at the IoC container
+
+		SpringApplication.run(MyappApplication.class, args); //Loads at IoC container in JVM
 	}
 
 }
