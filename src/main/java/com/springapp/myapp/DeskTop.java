050d32c945800e792f4d184f35c81d8c08d65427
azad buge <azad@azads-MacBook-Air.local>
2026-03-30 16:36:16 +0530

    using qualifier instead of the primary at field levelto correct the confusin

--- src/main/java/com/springapp/myapp/DeskTop.java
+++ src/main/java/com/springapp/myapp/DeskTop.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary // use of primary to solve the confusion of by type
 public class DeskTop implements ComputerInterFace {
     public void pc() {
         System.out.println("DeskTop");
--- src/main/java/com/springapp/myapp/Hello.java
+++ src/main/java/com/springapp/myapp/Hello.java
@@ -1,12 +1,14 @@
 package com.springapp.myapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Hello {
     
     @Autowired
+    @Qualifier("lapTop")
     private ComputerInterFace comp;
 
     public void build(){
